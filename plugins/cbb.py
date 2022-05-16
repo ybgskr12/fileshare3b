32c38e86c6f58f9b21feafe12d3eb37e8df04807
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-05-16 07:53:33 +0700

    Initial commit

---
+++ plugins/cbb.py
@@ -0,0 +1,27 @@
+# (©)Codexbotz
+# Recode by @mrismanaziz
+# t.me/SharingUserbot & t.me/Lunatic0de
+
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+
+from bot import Bot
+from config import CHANNEL1, CHANNEL2, GROUP, OWNER
+
+
+@Bot.on_callback_query()
+async def cb_handler(client: Bot, query: CallbackQuery):
+    data = query.data
+    if data == "about":
+        await query.message.edit_text(
+            text=f"<b>Tentang Bot ini:\n\n • ᴏᴡɴᴇʀ: @{OWNER}\n • ᴄʜᴀɴɴᴇʟ¹: @{CHANNEL1}\n • ᴄʜᴀɴɴᴇʟ²: @{CHANNEL2}\n • ɢʀᴏᴜᴘs: @{GROUP}\n • sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ: <a href='https://xnxx.com'>ᴋʟɪᴋ ᴅɪsɪɴɪ</a></b>\n",
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")]]
+            ),
+        )
+    elif data == "close":
+        await query.message.delete()
+        try:
+            await query.message.reply_to_message.delete()
+        except BaseException:
+            pass
